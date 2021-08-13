829437752967e488d1dd9685c80a6d1f927d08e6
Houssem Marzougui <marzouguih86@gmail.com>
2021-08-13 01:10:22 -0700

    NEXT APIs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
-import Layout from '../components/Layout'
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+
 export default function Home() {
+  getServerSideProps()
   return (
+
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
